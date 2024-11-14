85af47ea5de033c7247bfc9bdc818e654d5bbce5
陳彥琮 <111633751+KaungLong@users.noreply.github.com>
2024-11-14 14:17:08 +0800

    Lobby view UI (#1)
    * refactor: 優化baseView
    
    * feat: LobbyView UI

--- NinjaNight/NinjaNight/Lobby/LobbyView.swift
+++ NinjaNight/NinjaNight/Lobby/LobbyView.swift
@@ -9,29 +9,61 @@ struct LobbyView: View {
     @StateObject var viewModel = LobbyViewModel()
 
     var body: some View {
+        BaseView(title: "Lobby") {
+            LobbyContentView(signOut: viewModel.signOut)
+                .navigationBarHidden(true)
+                .onReceive(viewModel.$event) { event in
+                    guard let event = event else { return }
+                    switch event {
+                    case .signOutSuccess:
+                        navigationPathManager.path = NavigationPath()
+                    case .signOutFailure(let message):
+                        print("Sign-out failed: \(message)")
+                    }
+                }
+        }
+    }
+}
+
+struct LobbyContentView: View {
+    var signOut: () -> Void
+
+    var body: some View {
         VStack {
             Text("Welcome to the Lobby!")
                 .font(.largeTitle)
                 .padding()
-            Button("Sign Out", action: viewModel.signOut)
+            Button("開始新遊戲", action: {})
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+            Button("邀請碼加入", action: {})
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+            Button("房間列表", action: {})
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+            Button("玩家資料編輯", action: {})
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+            Button("登出", action: signOut)
                 .padding()
                 .background(Color.red)
                 .foregroundColor(.white)
                 .cornerRadius(8)
-            .padding()
-            .background(Color.red)
-            .foregroundColor(.white)
-            .cornerRadius(8)
         }
-        .navigationBarHidden(true)
-        .onReceive(viewModel.$event) { event in
-            guard let event = event else { return }
-            switch event {
-            case .signOutSuccess:
-                navigationPathManager.path = NavigationPath()
-            case .signOutFailure(let message):
-                print("Sign-out failed: \(message)")
-            }
-        }
+    }
+}
+
+struct LobbyContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LobbyContentView(signOut: {})
     }
 }
