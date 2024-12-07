c150fc3594dea0ee29a1a957d045a7521a41c7d9
Ozen <infinity19971023@gmail.com>
2024-12-07 19:47:26 +0800

    feat: GameView初期UI建置

---
+++ NinjaNight/NinjaNight/Game/Game.swift
@@ -0,0 +1,15 @@
+//
+//  Game.swift
+//  NinjaNight
+//
+//  Created by 陳彥琮 on 2024/12/5.
+//
+import SwiftUI
+
+class Game: ComposeObservableObject<Game.Event> {
+    
+    enum Event {
+        
+    }
+    
+}
